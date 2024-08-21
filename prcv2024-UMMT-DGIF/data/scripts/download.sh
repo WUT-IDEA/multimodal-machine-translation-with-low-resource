d542db28d8e26d8bc310d77967853c8799cb3a3f
WUT-IDEA <cathylilin@whut.edu.cn>
2024-08-21 15:42:50 +0800

    Update and rename 00-download.sh to download.sh

--- prcv2024-UMMT-DGIF/data/scripts/download.sh
+++ prcv2024-UMMT-DGIF/data/scripts/download.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CC_URL="https://zenodo.org/record/4646961/files/conceptual_captions_en_de.tar.bz2"
 M30K_FEATS_URL="https://zenodo.org/record/4646961/files/multi30k_oidv4_features.tar.xz"
 TF_OBJ_URL="https://zenodo.org/record/4646961/files/faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12.tar.bz2"
 
@@ -9,13 +8,6 @@ if [ ! -d "faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12_ghconfig_ree
     wget $TF_OBJ_URL
   fi
   tar xvf `basename $TF_OBJ_URL`
-fi
-
-if [ ! -f "conceptual_captions/cc-en-de.tsv.train" ]; then
-  if [ ! -f `basename $CC_URL` ]; then
-    wget $CC_URL
-  fi
-  tar xvf `basename $CC_URL`
 fi
 
 if [ ! -d "multi30k/features" ]; then
@@ -27,6 +19,7 @@ fi
 
 # Checkout Multi30k corpus
 if [ ! -d "multi30k/raw" ]; then
+  mkdir multi30k
   wget https://github.com/multi30k/dataset/archive/refs/heads/master.tar.gz
   tar xvf master.tar.gz -C multi30k --wildcards '*task1/raw/*.en.gz' --strip-components=3
   tar xvf master.tar.gz -C multi30k --wildcards '*task1/raw/*.de.gz' --strip-components=3
